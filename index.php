729f4baa0c160cabf6e850bdba996fc53b296d3b
DedenSetiawan1 <akunpribkoden19@gmail.com>
2024-08-27 11:21:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="submit.php" method="POST">
+  <label for="na  me">Nama:</label>
+  <input type="text" id="name" name="name">
+</br>
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email">
+  </br>
+  <label for="pesan">Pesan:</label>
+  <input type="pesan" id="pesan" name="pesan">
+  <button type="kirim">Kirim</button>
+</form>
+</form>
+</body>
+</html>
